6cab95e5e9b2d9d881e68d3423371ae535cb99b1
Arief Syaifuddin <asvelezer@gmail.com>
2021-06-12 13:40:30 +0700

    get api to compare

--- faceapi/views.py
+++ faceapi/views.py
@@ -3,8 +3,22 @@ from django.http import JsonResponse, HttpResponse, HttpRequest
 import requests
 from . import face_rec as frec
 # Create your views here.
+
+
 def index(request):
     return HttpResponse('Ini Index')
+
+
+def download_image(img_url, targetname=None):
+    filename = img_url.split('/')[-1]
+    dir = 'faces/'+filename
+    if targetname != None:
+        dir = 'faces/'+targetname
+    img = requests.get(img_url)
+    with open(dir, 'wb') as f:
+        f.write(img.content)
+    return dir
+
 
 def get_data_from_url(url):
     data = requests.get(url)
@@ -12,22 +26,15 @@ def get_data_from_url(url):
         data = data.json()
         data = data['data']
         for d in data:
-            d['foto'] = download_image(d['foto']) 
+            d['foto'] = download_image(d['foto'])
     return data
 
-def download_image(img_url):
-    filename = img_url.split('/')[-1]
-    dir = 'faces/'+filename
-    img = requests.get(img_url)
-    with open(dir, 'wb') as f:
-        f.write(img.content)
-    return dir
 
-def compare(request, img_path):
+def compare(request):
     url = 'http://localhost:8080/api/people'
     data = get_data_from_url(url)
     encoded_faces = frec.get_encoded_faces(data)
-    face_names = frec.classify_face(img_path, encoded_faces)
-    
-    
-    return HttpResponse(data)
+    img_url = request.GET['img']
+    face_names = frec.classify_face(download_image(img_url, 'test.jpg'), encoded_faces)
+
+    return HttpResponse(face_names)
